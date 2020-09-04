9a67bae25f3f95612d144e4305330fd67d7778f7
Aminatakabba <aminata-kabba@hotmail.com>
2020-09-04 20:43:40 +0100

    Updates

---
+++ precache-manifest.7aceb80f268a28736b768bc4039de884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f26522b7e3988f34e8d962e3b715dd2",
+    "url": "/wise_advice_reactapp/index.html"
+  },
+  {
+    "revision": "a2c1844e19aa4ced45bc",
+    "url": "/wise_advice_reactapp/static/css/main.7cfcb58f.chunk.css"
+  },
+  {
+    "revision": "a559f515c882b8f9f1c2",
+    "url": "/wise_advice_reactapp/static/js/2.56872f72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wise_advice_reactapp/static/js/2.56872f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c1844e19aa4ced45bc",
+    "url": "/wise_advice_reactapp/static/js/main.855e90f9.chunk.js"
+  },
+  {
+    "revision": "825a03b9a20dd6c8afd9",
+    "url": "/wise_advice_reactapp/static/js/runtime-main.a1b546ae.js"
+  },
+  {
+    "revision": "daac3ade90babba50f09e5e09444b8ae",
+    "url": "/wise_advice_reactapp/static/media/bg-img.daac3ade.jpg"
+  }
+]);
